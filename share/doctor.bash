e9e3adecfb119f0689ec4094f94bcc237a281383
Eric Saxby <sax@livinginthepast.org>
2023-05-02 09:37:48 -0700

    Rename files with .bash extension

--- share/doctor.bash
+++ share/doctor.bash
@@ -2,7 +2,7 @@
 
 #DIR=$(dirname $0);
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-source "$DIR/os.sh"
-source "$DIR/cecho.sh"
-source "$DIR/check.sh"
-source "$DIR/step.sh"
+source "$DIR/os.bash"
+source "$DIR/cecho.bash"
+source "$DIR/check.bash"
+source "$DIR/step.bash"
